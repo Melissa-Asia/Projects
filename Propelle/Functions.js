851e83aaebe4273c8b48ba1e42b1c759769854fe
Melissa Asiamah <Asiamah.m98@gmail.com>
2023-08-15 21:06:25 +0100

    JS edit

--- Propelle/Functions.js
+++ Propelle/Functions.js
@@ -6,8 +6,8 @@ function log() {
     let h2 = document.querySelector (".sub");
     let h3 = document.querySelector (".sub-title");
 h1.innerHTML = `Thanks ${Name} for sending!`;
-h2.innerHTML ="Please check your email: " + email;
-h3.innerHTML =`Remember your password ${password}!`;
+h2.innerHTML =`Please check your email:  ${email}`;
+h3.innerHTML =`Remember your password: ${password}!`;
 }
 let login = document.querySelector ("button");
 login.addEventListener("click", log);
